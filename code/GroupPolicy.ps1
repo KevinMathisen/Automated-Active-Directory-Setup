fb6bb32137f8dd2e4f58e446a622dcf3d6b69efe
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2022-03-13 17:10:14 +0100

    feat: add GroupPolicy.ps1

---
+++ code/GroupPolicy.ps1
@@ -0,0 +1,63 @@
+﻿# This Script will use Microsoft Security Baseline as a starting point for Group Policy
+# It will also create our own Group Policy object for Administration
+
+
+# Install 7zip for unpacking zip files (As recommended by Erik):
+choco install -y 7zip
+
+# Install Windows 10 Security baseline:
+Invoke-WebRequest -URI 'https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/Windows%2010%20version%2021H2%20Security%20Baseline.zip' -OutFile C:\Users\Administrator\Windows%2010%20version%2021H2%20Security%20Baseline.zip
+# Install Windows Server 2022 Security baseline:
+Invoke-WebRequest -URI 'https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/Windows%20Server%202022%20Security%20Baseline.zip' -OutFile C:\Users\Administrator\Windows%20Server%202022%20Security%20Baseline.zip
+
+# Unpack the zip files:
+7z x .\Windows%2010%20version%2021H2%20Security%20Baseline.zip
+7z x .\Windows%20Server%202022%20Security%20Baseline.zip
+
+# Run the powershell scripts downloaded to get the Group Policies:
+Set-Location '.\Windows-10-v21H2-Security-Baseline\Scripts\'
+.\Baseline-ADImport.ps1
+Set-Location ..
+Set-Location ..
+
+Set-Location '.\Windows Server-2022-Security-Baseline-FINAL\Scripts\'
+.\Baseline-ADImport.ps1
+Set-Location ..
+Set-Location ..
+
+
+# Link Group Policies from Windows 10 Security baseline to Clients:
+$OU = 'OU=Clients,DC=riot,DC=core'
+Get-GPO -Name "MSFT Internet Explorer 11 - Computer" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Internet Explorer 11 - User" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows 10 21H2 - BitLocker" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows 10 21H2 - Computer" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows 10 21H2 - Credential Guard" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows 10 21H2 - Defender Antivirus" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows 10 21H2 - Domain Security" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows 10 21H2 - User" | New-GPLink -Target $OU
+
+# Link Group Policies from Windows Server Security baseline to Domain Controller:
+$OU = 'OU=Domain Controllers,DC=riot,DC=core'
+Get-GPO -Name "MSFT Internet Explorer 11 - Computer" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Internet Explorer 11 - User" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows Server 2022 - Defender Antivirus" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows Server 2022 - Domain Controller" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows Server 2022 - Domain Security" | New-GPLink -Target $OU
+
+# Link Group Policies from Windows Server Security baseline to Servers:
+$OU = 'OU=Servers,DC=riot,DC=core'
+Get-GPO -Name "MSFT Internet Explorer 11 - Computer" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Internet Explorer 11 - User" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows Server 2022 - Defender Antivirus" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows Server 2022 - Member Server" | New-GPLink -Target $OU
+Get-GPO -Name "MSFT Windows Server 2022 - Domain Security" | New-GPLink -Target $OU
+
+
+
+# Import Selfmade GPO for Administration:
+# As we do not trust the administration with computers this group policy disables access to the control panel and command propt as recommended by https://www.netwrix.com/group_policy_best_practices.html
+Import-GPO -BackupGpoName U_AdmRestrictionPolicyBackUp -TargetName U_AdmRestrictionPolicy -CreateIfNeeded -Path C:\Users\Administrator\dcsg-1005-project\code\
+
+Get-GPO -Name "U_AdmRestrictionPolicy" | New-GPLink -Target "OU=Adm,OU=AllUsers,DC=riot,DC=core"
+
